dea62152e06c703306b664ee0e4c37a36e30d307
Tim Saylor <tim.saylor@gmail.com>
2015-02-19 20:33:10 -0600

    12 factor settings and such

--- ordclub/settings.py
+++ ordclub/settings.py
@@ -11,21 +11,50 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 import random
 import string
+import ConfigParser
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+# 12 factor config: http://www.wellfireinteractive.com/blog/easier-12-factor-django/
+def read_env():
+    """Reads local default environment variables from a file.
+        <BASE_DIR>/settings.conf
+    """
+    settings_conf_path = os.path.join(BASE_DIR, 'settings.conf')
+    if os.access(settings_conf_path, os.F_OK):
+        config = ConfigParser.ConfigParser()
+        config.read(settings_conf_path)
+        env = config.get('DEFAULT', 'env')
+        for key, value in config.items(env):
+            os.environ.setdefault(key, value)
+
+# load env variables from the conf file into settings
+read_env()
+
+
+def env_var(key, default=None, cache=False):
+    """Retrieves env vars and makes Python boolean replacements"""
+    val = os.environ.get(key, default)
+    if cache and val == default:
+        os.environ[key] = default
+    if val == 'True':
+        val = True
+    elif val == 'False':
+        val = False
+    return val
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''.join(random.choice(string.printable) for x in range(40))
-DEBUG = True
+SECRET_KEY = env_var('SECRET_KEY',
+                     default=''.join(random.choice(string.printable) for x in range(40)))
 
-TEMPLATE_DEBUG = True
+DEBUG = env_var('DEBUG', default=False)
+TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env_var('ALLOWED_HOSTS', default='').split(',')
 
 
 # Application definition
@@ -63,8 +92,7 @@ WSGI_APPLICATION = 'ordclub.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES = {}
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {'default': dj_database_url.config(default='sqlite:///db.sqlite')}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -93,7 +121,7 @@ STATICFILES_DIRS = (
 )
 
 # Twitter keys
-CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
-CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
-ACCESS_TOKEN_KEY = os.environ.get('ACCESS_TOKEN_KEY')
-ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
+CONSUMER_KEY = env_var('CONSUMER_KEY')
+CONSUMER_SECRET = env_var('CONSUMER_SECRET')
+ACCESS_TOKEN_KEY = env_var('ACCESS_TOKEN_KEY')
+ACCESS_TOKEN_SECRET = env_var('ACCESS_TOKEN_SECRET')
