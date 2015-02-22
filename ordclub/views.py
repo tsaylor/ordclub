e2b114fa6f309b3c8815f946bbc86242cf989209
Tim Saylor <tim.saylor@gmail.com>
2015-02-21 21:57:18 -0600

    Pull profiles from model instead of twitter api

--- ordclub/views.py
+++ ordclub/views.py
@@ -3,17 +3,14 @@ import twitter
 from django.conf import settings
 from django.shortcuts import render
 
+from profiles import models
+
 # https://pbs.twimg.com/profile_images/531250423391191041/ZeS6fUev_200x200.jpeg
 
 def home(request):
-    api = twitter.Api(consumer_key=settings.CONSUMER_KEY,
-                      consumer_secret=settings.CONSUMER_SECRET,
-                      access_token_key=settings.ACCESS_TOKEN_KEY,
-                      access_token_secret=settings.ACCESS_TOKEN_SECRET)
-    list_members = api.GetListMembers(
-        None, 'ord-campers', owner_screen_name='therealfitz')
+    list_members = models.Profile.objects.all()
     people = (dict(name=m.name,
-                   pic=m.profile_image_url.replace('_normal', '_bigger'),
+                   pic=m.user_json['profile_image_url'].replace('_normal', '_bigger'),
                    screen_name=m.screen_name)
               for m in list_members)
     return render(request, 'home.html', {'people': people})
