d1feebd6682e81edd7101fd805028512d9a90510
Tim Saylor <tim.saylor@gmail.com>
2015-02-28 17:19:54 -0600

    bypass custom view, since the page is static html now anyway

--- ordclub/urls.py
+++ ordclub/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'ordclub.views.home', name='home'),
+    #url(r'^$', 'ordclub.views.home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
     url(r'^api/', include('api.urls', namespace='api')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
