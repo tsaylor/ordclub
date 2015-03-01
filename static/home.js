b19df19c0e41ba9134614633b3a0adeae4642029
Tim Saylor <tim.saylor@gmail.com>
2015-03-01 15:00:00 -0600

    replace short profile with closer icon

--- static/home.js
+++ static/home.js
@@ -8,18 +8,19 @@ var delay = (function(){
 })();
 
 function shortlong() {
-    if ($(this).nextUntil('.short_profile').hasClass('hidden')) {
-        $('.long_profile').addClass('hidden')
-        $(this).nextUntil('.short_profile').removeClass('hidden') // only the next long profile
-    } else {
-        $(this).nextUntil('.short_profile').addClass('hidden') // only the next long profile
+    $('.long_profile').addClass('hidden');
+    var this_is_closed = $(this).hasClass('closer');
+    $('.short_profile').removeClass('closer');
+    if (!this_is_closed) {
+        $(this).nextUntil('.short_profile').removeClass('hidden'); // open the next long profile after 'this' short_profile
+        $(this).addClass('closer');
     }
 }
 
 last_search = 'dummy'
 
 function attach_click_handlers() {
-    $('.people li').click(shortlong);
+    $('.people li.short_profile').click(shortlong);
 }
 
 function render_profile(profile) {
